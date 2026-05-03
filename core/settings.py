2456df5b083e0cec3be7e638b2dafc6db81b98fb
sanvianand12 <sanvianand2021@gmail.com>
2026-05-03 22:23:13 +0530

    Final UI update

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-98@nyd(%lk*sow!-du(xn7p%hiki7$v^-1#krf-a^3oxdx$(5)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
